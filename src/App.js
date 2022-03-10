ddf8a01fa798ec5e8f11f38ed5f4451b29ff1080
Szymon <84242400+Szymon1993@users.noreply.github.com>
2022-03-10 21:18:54 +0100

    added routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import './App.scss';
 import Splash from "./components/1.splash,login,singUp/1.1.splash/1.1.splash";
 import Login from "./components/1.splash,login,singUp/1.2.login/1.2.login";
 
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 
 
 const  App = () => {
-  return ( <>
-    {/*<Splash/>*/}
-    <Login/>
-  </>);
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Splash/>
+        </Route>
+        <Route path="/login">
+          <Login/>
+        </Route>
+
+    </Switch>
+  </Router>);
 }
 
 export default App;
