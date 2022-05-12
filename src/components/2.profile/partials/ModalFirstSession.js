9422fdd3434a64ee048f91d1e8b8b937a372a965
SzymonGonerko <szymon.gonerko@gmail.com>
2022-05-12 22:42:38 +0200

    refactoring: modyficated text

--- src/components/2.profile/partials/ModalFirstSession.js
+++ src/components/2.profile/partials/ModalFirstSession.js
@@ -19,7 +19,7 @@ const thanksText = 'Cześć! Jest mi miło, że tu jesteś...'
 const posibilityText = 'Przed Tobą kilka niespodzianek. Zobaczysz animację składającą się z 2500 małych obiektów przypominających Ciebie. Poznasz innych użytkowników, a także  nawiążesz z nimi kontakt...'
 const rememberText = ' Jednak przedtem, pamiętaj o kilku rzeczach...'
 const firstHintText = 'Wstaw swoje zdjęcie, najlepiej portretowe. Animacja zdjęć jest tajemnicza. Dzięki temu kluczowe w poznawaniu innych są historie, a portrety pozostają w swerze Twojej fantazji...💫'
-const secHintText = 'Napisz swoją historię. Zadbaj aby była długa i piękna. Potem przejdź do strony głównej 🏠. Życzę Ci wiele miłych chwil... '
+const secHintText = 'Wybierz jedno z pytań i napisz swoją historię. Potem przejdź do strony głównej 🏠. Życzę Ci wiele miłych chwil... '
 const PS = 'PS. Vanilla-Date jest młoda więc ma mało użytkowników. Proszę, zajrzyj tu następnego dnia...'
 
 const intro = new Audio(audio)
